1888beda1f21b4ffe5335235cbf08459e50b8538
AbdiazizMuktar <muktarabdiaziz12@gmail.com>
2023-09-07 11:36:06 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 /**
- * C is easy
- */
+ * main - c programming is awesome
+ * Return: Always 0 (success)
+*/
+int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
-return(0)
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
