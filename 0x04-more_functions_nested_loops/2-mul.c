c4dc6dce79b629ed11e8682e74ccd4e4b08bff63
AbdiazizMuktar <muktarabdiaziz12@gmail.com>
2023-09-14 07:44:47 -0700

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - used for multiplication of two integers
+ * @a: is an integer
+ * @b: is an integer
+ * Return: always 0.
+*/
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	printf("%d * %d = %d", a, b, result);
+
+	return (0);
+}
