1cfc292cfb77f141ae799e446dcd176419a036a8
AbdiazizMuktar <muktarabdiaziz12@gmail.com>
2023-09-20 11:00:23 -0700

    0-strcat,c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,8 +2,8 @@
 
 /**
  *_strcat - a function that combines strings
- * @*dest: a string
- * @*src: another string
+ * @dest: a string
+ * @src: another string
  *
  * Return: always 0
  */
@@ -11,10 +11,10 @@
 char *_strcat(char *dest, char *src)
 {
 	int j, k;
-	
+
 	j = 0;
 	k = 0;
-	 
+
 	while (dest[j] != '\0')
 	{
 		j++;
@@ -23,10 +23,9 @@ char *_strcat(char *dest, char *src)
 	{
 		dest[j] = src[k];
 		k++;
-		j++;	
+		j++;
 	}
 	dest[j] = '\0';
 
 	return (dest);
 }
-	
