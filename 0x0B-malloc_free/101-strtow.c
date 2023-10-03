8262cab711d1503a524f8e34dd2d5d08818e2f13
Codeniaaz <muktarabdiaziz12@gmail.com>
2023-10-03 13:03:52 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,106 +1,127 @@
 #include "main.h"
 #include <stdlib.h>
 
-int is_space(char c) {
-    return (c == ' ' || c == '\t' || c == '\n' || c == '\r');
+int is_space(char c)
+{
+	return (c == ' ' || c == '\t' || c == '\n' || c == '\r');
 }
 
-int countWords(char *str) {
-    int count = 0;
-    int inWord = 0;
+int countWords(char *str)
+{
+	int count = 0;
+	int inWord = 0;
 
-    while (*str) {
-        if (is_space(*str)) {
-            inWord = 0;
-        } else if (!inWord) {
-            inWord = 1;
-            count++;
-        }
-        str++;
-    }
+	while (*str)
+	{
+		if (is_space(*str))
+		{
+			inWord = 0;
+		}
+		else if (!inWord)
+		{
+			inWord = 1;
+			count++;
+		}
+	str++;
+	}
 
-    return count;
+	return (count);
 }
 
-char **strtow(char *str) {
-    
+char **strtow(char *str)
+{   
 	int numWords = countWords(str);
 	int wordIndex = 0;
-    int wordLength = 0;
-    char *wordStart = NULL;	
+	int wordLength = 0;
+	char *wordStart = NULL;	
 	int i;	
 	char **words;
 
-	if (str == NULL || *str == '\0') {
-        return NULL;
-    }
+	if (str == NULL || *str == '\0')
+	{
+		return (NULL);
+	}
 
-    words = (char **)malloc((numWords + 1) * sizeof(char *));
-    if (words == NULL) {
-        return NULL;
-    }
+	words = (char **)malloc((numWords + 1) * sizeof(char *));
+	if (words == NULL)
+	{
+		return (NULL);
+	}
 
-    while (*str) {
-        if (is_space(*str)) {
-            if (wordLength > 0) {
-                words[wordIndex] = (char *)malloc((wordLength + 1) * sizeof(char));
-                if (words[wordIndex] == NULL) {
-                    for (i = 0; i < wordIndex; i++) {
-                        free(words[i]);
-                    }
-                    free(words);
-                    return NULL;
-                }
+	while (*str)
+	{
+		if (is_space(*str))
+		{
+			if (wordLength > 0)
+			{
+				words[wordIndex] = (char *)malloc((wordLength + 1) * sizeof(char));
+				if (words[wordIndex] == NULL)
+				{
+					for (i = 0; i < wordIndex; i++)
+					{
+						free(words[i]);
+					}
+					free(words);
+					return (NULL);
+				}
 
-                for (i = 0; i < wordLength; i++) {
-                    words[wordIndex][i] = wordStart[i];
-                }
-                words[wordIndex][wordLength] = '\0';
-                wordIndex++;
-                wordLength = 0;
-            }
-        } else {
-            if (wordLength == 0) {
-                wordStart = str;
-            }
-            wordLength++;
-        }
+				for (i = 0; i < wordLength; i++)
+				{
+					words[wordIndex][i] = wordStart[i];
+				}
+				words[wordIndex][wordLength] = '\0';
+				wordIndex++;
+				wordLength = 0;
+			}
+		}
+		else
+		{
+			if (wordLength == 0)
+			{
+				wordStart = str;
+			}
+			wordLength++;
+		}
 
-        str++;
-    }
+	str++;
+	}
 
-    if (wordLength > 0) {
-        words[wordIndex] = (char *)malloc((wordLength + 1) * sizeof(char));
-        if (words[wordIndex] == NULL) {
-            for (i = 0; i <= wordIndex; i++) {
-                free(words[i]);
-            }
-            free(words);
-            return NULL;
-        }
+	if (wordLength > 0)
+	{
+		words[wordIndex] = (char *)malloc((wordLength + 1) * sizeof(char));
+		if (words[wordIndex] == NULL)
+		{
+			for (i = 0; i <= wordIndex; i++)
+			{
+				free(words[i]);
+			}
+			free(words);
+			return (NULL);
+		}
 
-        for (i = 0; i < wordLength; i++) {
-            words[wordIndex][i] = wordStart[i];
-        }
-        words[wordIndex][wordLength] = '\0';
-        wordIndex++;
-    }
-
-    words[wordIndex] = NULL;
-
-    return words;
+		for (i = 0; i < wordLength; i++)
+		{
+			words[wordIndex][i] = wordStart[i];
+		}
+		words[wordIndex][wordLength] = '\0';
+		wordIndex++;
+	}
+	words[wordIndex] = NULL;
+	return (words);
 }
 
-void freeWords(char **words) {
+void freeWords(char **words)
+{
 	int i;
 
-    if (words == NULL) {
-        return;
-    }
+	if (words == NULL)
+	{
+		return;
+	}
 
-    for (i = 0; words[i] != NULL; i++) {
-        free(words[i]);
-    }
-
+	for (i = 0; words[i] != NULL; i++)
+	{
+		free(words[i]);
+	}
 	free(words);
 }
