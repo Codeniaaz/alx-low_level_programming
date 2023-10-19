702a8e89313b420b237d61380c15ab0ef8e9875e
codeniaaz <muktarabdiaziz12@gmail.com>
2023-10-19 13:25:06 -0700

    function that prints be4 the main is executed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,9 +3,9 @@
 /**
  * my_constructor - prints before main is executed
  */
-void my_constructor() __attribute__((constructor));
+void my_constructor(void) __attribute__((constructor));
 
-void my_constructor()
+void my_constructor(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
