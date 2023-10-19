51f95564239044c4f5f216bdb50f146d354ddf77
codeniaaz <muktarabdiaziz12@gmail.com>
2023-10-19 11:47:17 -0700

    adds a new node at the beginning of a list_t list.

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,9 +1,10 @@
 #include "lists.h"
 
 /**
- *
- *
- *
+ *add_node -  adds a new node at the beginning of a list_t list.
+ *@head: head
+ *@str: string
+ *Return: always success
  */
 list_t *add_node(list_t **head, const char *str)
 {
