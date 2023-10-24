02c685a12f88568a4d0731cf237d76acf66e83f5
codeniaaz <muktarabdiaziz12@gmail.com>
2023-10-24 00:41:20 -0700

    added a get node function at nth index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,10 +9,10 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
-	
+
 	while (head != NULL)
 	{
-        	if (i == index)
+		if (i == index)
 			return (head);
 		head = head->next;
 		i++;
