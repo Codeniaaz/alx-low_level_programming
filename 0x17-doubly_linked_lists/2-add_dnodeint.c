d929376d92669eba4fb205f3e06659b9d713855c
Codeniaaz <muktarabdiaziz12@gmail.com>
2023-12-07 15:26:28 +0300

    DLL

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -3,6 +3,7 @@
 /**
  *add_dnodeint - adds a new node at the beginning of a dlistint_t list
  *@head: head pointer
+ *@n: the data
  *Return: Null if it failed else the new node
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
